c6abb6e11b0828c0c3a45732efcc4a512403cf15
DOHA <dohae243@gmail.com>
2016-09-25 13:08:04 +0200

    change ports

--- spring-security-oauth-ui-password/src/test/java/org/baeldung/live/AuthorizationLiveTest.java
+++ spring-security-oauth-ui-password/src/test/java/org/baeldung/live/AuthorizationLiveTest.java
@@ -28,11 +28,11 @@ public class AuthorizationLiveTest {
     public void givenUser_whenUseFooClient_thenOkForFooResourceOnly() {
         final String accessToken = obtainAccessToken("fooClientIdPassword", "john", "123");
 
-        final Response fooResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8081/spring-security-oauth-resource/foos/1");
+        final Response fooResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8082/spring-security-oauth-resource/foos/1");
         assertEquals(200, fooResponse.getStatusCode());
         assertNotNull(fooResponse.jsonPath().get("name"));
 
-        final Response barResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8081/spring-security-oauth-resource/bars/1");
+        final Response barResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8082/spring-security-oauth-resource/bars/1");
         assertEquals(403, barResponse.getStatusCode());
     }
 
@@ -40,10 +40,10 @@ public class AuthorizationLiveTest {
     public void givenUser_whenUseBarClient_thenOkForBarResourceReadOnly() {
         final String accessToken = obtainAccessToken("barClientIdPassword", "john", "123");
 
-        final Response fooResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8081/spring-security-oauth-resource/foos/1");
+        final Response fooResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8082/spring-security-oauth-resource/foos/1");
         assertEquals(403, fooResponse.getStatusCode());
 
-        final Response barReadResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8081/spring-security-oauth-resource/bars/1");
+        final Response barReadResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8082/spring-security-oauth-resource/bars/1");
         assertEquals(200, barReadResponse.getStatusCode());
         assertNotNull(barReadResponse.jsonPath().get("name"));
 
@@ -55,10 +55,10 @@ public class AuthorizationLiveTest {
     public void givenAdmin_whenUseBarClient_thenOkForBarResourceReadWrite() {
         final String accessToken = obtainAccessToken("barClientIdPassword", "tom", "111");
 
-        final Response fooResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8081/spring-security-oauth-resource/foos/1");
+        final Response fooResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8082/spring-security-oauth-resource/foos/1");
         assertEquals(403, fooResponse.getStatusCode());
 
-        final Response barResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8081/spring-security-oauth-resource/bars/1");
+        final Response barResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8082/spring-security-oauth-resource/bars/1");
         assertEquals(200, barResponse.getStatusCode());
         assertNotNull(barResponse.jsonPath().get("name"));
 
