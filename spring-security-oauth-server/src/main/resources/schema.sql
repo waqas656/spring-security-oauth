229a3b8e04673935df78f9238eac6ccb7ee50b0d
michal_aibin <michal.aibin@gmail.com>
2016-12-08 21:49:33 +0100

    Rollback and fixes

--- spring-security-oauth-server/src/main/resources/schema.sql
+++ spring-security-oauth-server/src/main/resources/schema.sql
@@ -63,14 +63,3 @@ create table if not exists ClientDetails (
   additionalInformation VARCHAR(4096),
   autoApproveScopes VARCHAR(255)
 );
-
-create table if not exists users (
-  username varchar(256),
-  password varchar(256),
-  enabled boolean
-);
-
-create table if not exists authorities (
-  username varchar(256),
-  authority varchar(256)
-);
