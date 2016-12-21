ecc8e8c42e678ad0ca67ea34bbb9fd5342d09553
eugenp <hanriseldon@gmail.com>
2016-12-21 12:38:44 +0200

    minor cleanup

--- spring-security-oauth-ui-password/src/test/java/org/baeldung/live/AuthorizationLiveTest.java
+++ spring-security-oauth-ui-password/src/test/java/org/baeldung/live/AuthorizationLiveTest.java
@@ -14,16 +14,6 @@ import com.jayway.restassured.response.Response;
 
 public class AuthorizationLiveTest {
 
-    private String obtainAccessToken(String clientId, String username, String password) {
-        final Map<String, String> params = new HashMap<String, String>();
-        params.put("grant_type", "password");
-        params.put("client_id", clientId);
-        params.put("username", username);
-        params.put("password", password);
-        final Response response = RestAssured.given().auth().preemptive().basic(clientId, "secret").and().with().params(params).when().post("http://localhost:8081/spring-security-oauth-server/oauth/token");
-        return response.jsonPath().getString("access_token");
-    }
-
     @Test
     public void givenUser_whenUseFooClient_thenOkForFooResourceOnly() {
         final String accessToken = obtainAccessToken("fooClientIdPassword", "john", "123");
@@ -35,7 +25,7 @@ public class AuthorizationLiveTest {
         final Response barResponse = RestAssured.given().header("Authorization", "Bearer " + accessToken).get("http://localhost:8082/spring-security-oauth-resource/bars/1");
         assertEquals(403, barResponse.getStatusCode());
     }
-    
+
     @Test
     public void givenUser_whenUseBarClient_thenOkForBarResourceReadOnly() {
         final String accessToken = obtainAccessToken("barClientIdPassword", "john", "123");
@@ -65,6 +55,18 @@ public class AuthorizationLiveTest {
         final Response barWritResponse = RestAssured.given().contentType(MediaType.APPLICATION_JSON_VALUE).header("Authorization", "Bearer " + accessToken).body("{\"id\":1,\"name\":\"MyBar\"}").post("http://localhost:8082/spring-security-oauth-resource/bars");
         assertEquals(201, barWritResponse.getStatusCode());
         assertEquals("MyBar", barWritResponse.jsonPath().get("name"));
+    }
+
+    //
+
+    private String obtainAccessToken(String clientId, String username, String password) {
+        final Map<String, String> params = new HashMap<String, String>();
+        params.put("grant_type", "password");
+        params.put("client_id", clientId);
+        params.put("username", username);
+        params.put("password", password);
+        final Response response = RestAssured.given().auth().preemptive().basic(clientId, "secret").and().with().params(params).when().post("http://localhost:8081/spring-security-oauth-server/oauth/token");
+        return response.jsonPath().getString("access_token");
     }
 
 }
