5292cb281af591da8f6b25f959ed8aedb3b3fb77
Loredana <loredana.crusoveanu@yahoo.com>
2017-03-06 14:34:57 +0200

    update boot version

--- spring-security-oauth-server/src/main/java/org/baeldung/mvc/config/WebSecurityConfig.java
+++ spring-security-oauth-server/src/main/java/org/baeldung/mvc/config/WebSecurityConfig.java
@@ -7,8 +7,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.core.annotation.Order;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+ 
 @Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
