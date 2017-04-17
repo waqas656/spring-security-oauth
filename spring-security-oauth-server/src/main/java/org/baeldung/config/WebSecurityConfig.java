ffa8b894bc496685d135bd45aa32defbe26620c4
lor6 <loredana.crusoveanu@yahoo.com>
2017-04-17 22:18:21 +0300

    Bael 383 (#35)
    * move token controller, update cors filter
    
    * update revoke token
    
    * fix conflict
    
    * add delete method
    
    * formatting

--- spring-security-oauth-server/src/main/java/org/baeldung/config/WebSecurityConfig.java
+++ spring-security-oauth-server/src/main/java/org/baeldung/config/WebSecurityConfig.java
@@ -35,6 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // @formatter:off
 		http.authorizeRequests().antMatchers("/login").permitAll()
 		.antMatchers("/oauth/token/revoke/**").permitAll()
+		.antMatchers("/oauth/token/revokeById/**").permitAll()
 		.antMatchers("/tokens/**").permitAll()
 		.anyRequest().authenticated()
 		.and().formLogin().permitAll()
