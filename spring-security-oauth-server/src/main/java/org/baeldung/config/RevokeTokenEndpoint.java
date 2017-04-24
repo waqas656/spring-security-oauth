edfefa50e544f5c58d3c552999ee602612836a1b
eugenp <hanriseldon@gmail.com>
2017-04-24 18:02:42 +0300

    cleanup work

--- spring-security-oauth-server/src/main/java/org/baeldung/config/RevokeTokenEndpoint.java
+++ spring-security-oauth-server/src/main/java/org/baeldung/config/RevokeTokenEndpoint.java
@@ -3,13 +3,12 @@ package org.baeldung.config;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@FrameworkEndpoint
+// @FrameworkEndpoint
 public class RevokeTokenEndpoint {
 
     @Resource(name = "tokenServices")
