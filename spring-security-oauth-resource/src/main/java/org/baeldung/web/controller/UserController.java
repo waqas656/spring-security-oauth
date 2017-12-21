553d94fc6507400a8caf84e42e7f7ecd76d146e8
DOHA <dohae243@gmail.com>
2017-12-21 22:57:46 +0200

    modify user controller

--- spring-security-oauth-resource/src/main/java/org/baeldung/web/controller/UserController.java
+++ spring-security-oauth-resource/src/main/java/org/baeldung/web/controller/UserController.java
@@ -2,12 +2,9 @@ package org.baeldung.web.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,16 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
-    @Autowired
-    private TokenStore tokenStore;
 
     @PreAuthorize("#oauth2.hasScope('read')")
     @RequestMapping(method = RequestMethod.GET, value = "/users/extra")
     @ResponseBody
-    public Map<String, Object> getExtraInfo(OAuth2Authentication auth) {
-        final OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) auth.getDetails();
-        final OAuth2AccessToken accessToken = tokenStore.readAccessToken(details.getTokenValue());
-        System.out.println(accessToken);
-        return accessToken.getAdditionalInformation();
+    public Map<String, Object> getExtraInfo(Authentication auth) {
+        OAuth2AuthenticationDetails oauthDetails = (OAuth2AuthenticationDetails) auth.getDetails();
+        Map<String, Object> details = (Map<String, Object>) oauthDetails.getDecodedDetails();
+        System.out.println("User organization is " + details.get("organization"));
+        return details;
     }
 }
