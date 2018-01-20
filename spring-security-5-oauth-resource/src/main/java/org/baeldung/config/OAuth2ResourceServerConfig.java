eeec4cf409e5b484b0931ed270d5799a78230bbb
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2018-01-20 10:14:53 +0200

    fix dependency versions

--- spring-security-5-oauth-resource/src/main/java/org/baeldung/config/OAuth2ResourceServerConfig.java
+++ spring-security-5-oauth-resource/src/main/java/org/baeldung/config/OAuth2ResourceServerConfig.java
@@ -1,12 +1,17 @@
 package org.baeldung.config;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -24,70 +29,66 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    @Override
-    public void configure(final HttpSecurity http) throws Exception {
-        http.sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-            .and()
-            .authorizeRequests()
-            .anyRequest()
-            .permitAll();
+	@Override
+	public void configure(final HttpSecurity http) throws Exception {
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and().authorizeRequests()
+				.anyRequest().permitAll();
 
-    }
+	}
 
-    // JWT token store
+	// JWT token store
 
-    @Override
-    public void configure(final ResourceServerSecurityConfigurer config) {
-        config.tokenServices(tokenServices());
-    }
+	@Override
+	public void configure(final ResourceServerSecurityConfigurer config) {
+		config.tokenServices(tokenServices());
+	}
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
 
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("123");
-        converter.setJwtClaimsSetVerifier(jwtClaimsSetVerifier());
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setSigningKey("123");
+		converter.setJwtClaimsSetVerifier(jwtClaimsSetVerifier());
 
-        // final Resource resource = new ClassPathResource("public.txt");
-        // String publicKey = null;
-        // try {
-        // publicKey = IOUtils.toString(resource.getInputStream());
-        // } catch (final IOException e) {
-        // throw new RuntimeException(e);
-        // }
-        // converter.setVerifierKey(publicKey);
-        return converter;
-    }
+		final Resource resource = new ClassPathResource("public.txt");
+		String publicKey = null;
+		try {
+			publicKey = IOUtils.toString(resource.getInputStream(), Charset.defaultCharset());
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
+		converter.setVerifierKey(publicKey);
+		return converter;
+	}
 
-    @Bean
-    public JwtClaimsSetVerifier jwtClaimsSetVerifier() {
-        return new DelegatingJwtClaimsSetVerifier(Arrays.asList(issuerClaimVerifier(), customJwtClaimVerifier()));
-    }
+	@Bean
+	public JwtClaimsSetVerifier jwtClaimsSetVerifier() {
+		return new DelegatingJwtClaimsSetVerifier(Arrays.asList(issuerClaimVerifier(), customJwtClaimVerifier()));
+	}
 
-    @Bean
-    public JwtClaimsSetVerifier issuerClaimVerifier() {
-        try {
-            return new IssuerClaimVerifier(new URL("http://localhost:8081"));
-        } catch (final MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@Bean
+	public JwtClaimsSetVerifier issuerClaimVerifier() {
+		try {
+			return new IssuerClaimVerifier(new URL("http://localhost:8081"));
+		} catch (final MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    @Bean
-    public JwtClaimsSetVerifier customJwtClaimVerifier() {
-        return new CustomClaimVerifier();
-    }
+	@Bean
+	public JwtClaimsSetVerifier customJwtClaimVerifier() {
+		return new CustomClaimVerifier();
+	}
 
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
-    }
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		return defaultTokenServices;
+	}
 }
