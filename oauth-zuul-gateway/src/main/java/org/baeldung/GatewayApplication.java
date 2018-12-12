887780dad4c9f808830ca146cbb16454d7ccdcc5
GetWordsDone <github@getwordsdone.com>
2018-12-12 07:40:59 +0530

    BAEL-1359 Initial

---
+++ oauth-zuul-gateway/src/main/java/org/baeldung/GatewayApplication.java
@@ -0,0 +1,16 @@
+package org.baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableResourceServer
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
