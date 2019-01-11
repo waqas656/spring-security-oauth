5f2f02d02373e6e2f0b8a16272273a3788c30300
GetWordsDone <github@getwordsdone.com>
2019-01-11 10:58:24 +0530

    Moving JWT key to spring-security-oauth2-autoconfigure

--- oauth-zuul-gateway/src/main/java/org/baeldung/Config/GatewayConfiguration.java
+++ oauth-zuul-gateway/src/main/java/org/baeldung/Config/GatewayConfiguration.java
@@ -24,39 +24,5 @@ public class GatewayConfiguration extends ResourceServerConfigurerAdapter {
             permitAll().
             antMatchers("/**").
             authenticated();
-    }
-
-    @Override
-    public void configure(final ResourceServerSecurityConfigurer config) {
-        config.tokenServices(tokenServices());
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-
-        converter.setSigningKey("123");
-        // final Resource resource = new ClassPathResource("public.txt");
-        // String publicKey = null;
-        // try {
-        // publicKey = IOUtils.toString(resource.getInputStream());
-        // } catch (final IOException e) {
-        // throw new RuntimeException(e);
-        // }
-        // converter.setVerifierKey(publicKey);
-        return converter;
-    }
-
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
-    }
+    }    
 }
--- oauth-zuul-gateway/src/main/java/org/baeldung/GatewayApplication.java
+++ oauth-zuul-gateway/src/main/java/org/baeldung/GatewayApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @EnableResourceServer
 @EnableZuulProxy
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
