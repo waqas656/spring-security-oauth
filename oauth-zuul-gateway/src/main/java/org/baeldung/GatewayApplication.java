aba95464f9e263afc270ce986cdeed656cb1140b
DOHA <dohae243@gmail.com>
2019-03-26 14:57:36 +0200

    upgrade zulu version

--- oauth-zuul-gateway/src/main/java/org/baeldung/GatewayApplication.java
+++ oauth-zuul-gateway/src/main/java/org/baeldung/GatewayApplication.java
@@ -3,9 +3,7 @@ package org.baeldung;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @EnableResourceServer
 @EnableZuulProxy
