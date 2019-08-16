33169dbd8c2e167e183de60acbeafc13efccd870
Dhawal Kapil <dhawalkapil@gmail.com>
2019-08-17 01:11:47 +0530

    BAEL-16781 Moving top articles to their respective packages

--- oauth-resource-server-2/src/main/java/com/baeldung/swagger2/SwaggerConfig.java
+++ oauth-resource-server-2/src/main/java/com/baeldung/swagger2/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.baeldung.config;
+package com.baeldung.swagger2;
 
 import java.util.Arrays;
 
@@ -31,27 +31,50 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().securitySchemes(Arrays.asList(securityScheme())).securityContexts(Arrays.asList(securityContext()));
+        return new Docket(DocumentationType.SWAGGER_2).select()
+        		.apis(RequestHandlerSelectors.any())
+        		.paths(PathSelectors.any())
+        		.build()
+        		.securitySchemes(Arrays.asList(securityScheme()))
+        		.securityContexts(Arrays.asList(securityContext()));
     }
 
     @Bean
     public SecurityConfiguration security() {
-        return SecurityConfigurationBuilder.builder().clientId(CLIENT_ID).clientSecret(CLIENT_SECRET).useBasicAuthenticationWithAccessCodeGrant(true).build();
+        return SecurityConfigurationBuilder.builder()
+        		.clientId(CLIENT_ID)
+        		.clientSecret(CLIENT_SECRET)
+        		.useBasicAuthenticationWithAccessCodeGrant(true)
+        		.build();
     }
 
     private SecurityScheme securityScheme() {
-        GrantType grantType = new AuthorizationCodeGrantBuilder().tokenEndpoint(new TokenEndpoint(AUTH_SERVER + "/token", "oauthtoken")).tokenRequestEndpoint(new TokenRequestEndpoint(AUTH_SERVER + "/authorize", CLIENT_ID, CLIENT_ID)).build();
+        GrantType grantType = new AuthorizationCodeGrantBuilder()
+        		.tokenEndpoint(new TokenEndpoint(AUTH_SERVER + "/token", "oauthtoken"))
+        		.tokenRequestEndpoint(
+        		  new TokenRequestEndpoint(AUTH_SERVER + "/authorize", CLIENT_ID, CLIENT_ID))
+        		.build();
 
-        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth").grantTypes(Arrays.asList(grantType)).scopes(Arrays.asList(scopes())).build();
+        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+        		.grantTypes(Arrays.asList(grantType))
+        		.scopes(Arrays.asList(scopes()))
+        		.build();
         return oauth;
     }
 
     private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(Arrays.asList(new SecurityReference("spring_oauth", scopes()))).forPaths(PathSelectors.regex("/foos.*")).build();
+        return SecurityContext.builder()
+        		.securityReferences(
+        		  Arrays.asList(new SecurityReference("spring_oauth", scopes())))
+        		.forPaths(PathSelectors.regex("/foos.*"))
+        		.build();
     }
 
     private AuthorizationScope[] scopes() {
-        AuthorizationScope[] scopes = { new AuthorizationScope("read", "for read operations"), new AuthorizationScope("write", "for write operations"), new AuthorizationScope("foo", "Access foo API") };
+        AuthorizationScope[] scopes = { 
+          new AuthorizationScope("read", "for read operations"), 
+          new AuthorizationScope("write", "for write operations"), 
+          new AuthorizationScope("foo", "Access foo API") };
         return scopes;
     }
 }
