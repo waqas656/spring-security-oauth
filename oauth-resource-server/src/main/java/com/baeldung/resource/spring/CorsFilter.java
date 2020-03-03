5a99d824c1563c822253f1721117dcc50d1abe09
sampadawagde <sampadawagde@localhost>
2020-03-03 18:24:27 +0530

    JAVA-406 : Added auth code live test and modified security config

--- oauth-resource-server/src/main/java/com/baeldung/resource/spring/CorsFilter.java
+++ oauth-resource-server/src/main/java/com/baeldung/resource/spring/CorsFilter.java
@@ -23,7 +23,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8086/");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Max-Age", "3600");
