16f7314ede8001fd3f4bbdd3c33ac0d989863ce3
sampadawagde <sampada.wagde@gmail.com>
2020-03-26 23:27:01 +0530

    JAVA-407: Added custom ClaimSetConverter to the resource server

--- oauth-jwt/jwt-resource-server/src/main/java/com/baeldung/jwt/spring/SecurityConfig.java
+++ oauth-jwt/jwt-resource-server/src/main/java/com/baeldung/jwt/spring/SecurityConfig.java
@@ -14,16 +14,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {// @formatter:off
-		http.cors()
-        .and().authorizeRequests().antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
+		http.cors().and().authorizeRequests().antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
 				.hasAuthority("SCOPE_read").antMatchers(HttpMethod.POST, "/api/foos").hasAuthority("SCOPE_write")
 				.anyRequest().authenticated().and().oauth2ResourceServer().jwt();
 	}// @formatter:on
 
 	@Bean
-	public JwtDecoder customDecoder(OAuth2ResourceServerProperties properties) {
+	JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
 		NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri()).build();
-
+		jwtDecoder.setClaimSetConverter(new OrganizationSubClaimAdapter());
 		return jwtDecoder;
 	}
 }
