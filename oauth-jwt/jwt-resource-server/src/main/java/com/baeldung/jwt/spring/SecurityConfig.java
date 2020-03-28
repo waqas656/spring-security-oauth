45fcc5d14b23e3448f274be9b57265a70df54df4
sampadawagde <sampada.wagde@gmail.com>
2020-03-28 15:00:07 +0530

    JAVA-407: improved formatting

--- oauth-jwt/jwt-resource-server/src/main/java/com/baeldung/jwt/spring/SecurityConfig.java
+++ oauth-jwt/jwt-resource-server/src/main/java/com/baeldung/jwt/spring/SecurityConfig.java
@@ -14,9 +14,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {// @formatter:off
-		http.cors().and().authorizeRequests().antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
-				.hasAuthority("SCOPE_read").antMatchers(HttpMethod.POST, "/api/foos").hasAuthority("SCOPE_write")
-				.anyRequest().authenticated().and().oauth2ResourceServer().jwt();
+		http.cors()
+        .and()
+          .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
+              .hasAuthority("SCOPE_read")
+            .antMatchers(HttpMethod.POST, "/api/foos")
+              .hasAuthority("SCOPE_write")
+            .anyRequest()
+              .authenticated()
+        .and()
+          .oauth2ResourceServer()
+            .jwt();
 	}// @formatter:on
 
 	@Bean
