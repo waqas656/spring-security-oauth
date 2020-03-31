38c14afff06bc8d26aa24047636e2c8b1a1ac891
sampadawagde <sampada.wagde@gmail.com>
2020-03-31 13:07:39 +0530

    JAVA-1132: Removed unused folder newstack

--- newstack/new-resource-server/src/main/java/com/baeldung/newstack/persistence/repository/IProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.newstack.persistence.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.baeldung.newstack.persistence.model.Project;
-
-public interface IProjectRepository extends PagingAndSortingRepository<Project, Long> {
-}
