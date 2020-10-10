87e960169d9102554559e6eaa01fcf594a83e1a7
Gerardo Roza <rozagerardo@gmail.com>
2020-10-10 12:10:16 -0300

    Upgraded Keycloak to version 11.0.2, adapted classes and configurations

--- oauth-jwt/jwt-auth-server/src/main/java/com/baeldung/jwt/config/Resteasy3Provider.java
+++ oauth-jwt/jwt-auth-server/src/main/java/com/baeldung/jwt/config/Resteasy3Provider.java
@@ -6,28 +6,32 @@ import org.keycloak.common.util.ResteasyProvider;
 
 public class Resteasy3Provider implements ResteasyProvider {
 
-	@Override
-	public <R> R getContextData(Class<R> type) {
-		ResteasyProviderFactory.getInstance();
-		return ResteasyProviderFactory.getContextData(type);
-	}
+    @Override
+    public <R> R getContextData(Class<R> type) {
+        ResteasyProviderFactory.getInstance();
+        return ResteasyProviderFactory.getContextData(type);
+    }
 
-	@Override
-	public void pushDefaultContextObject(Class type, Object instance) {
-		ResteasyProviderFactory.getInstance();
-		ResteasyProviderFactory.getContextData(Dispatcher.class).getDefaultContextObjects().put(type, instance);
-	}
+    @SuppressWarnings("rawtypes")
+    @Override
+    public void pushDefaultContextObject(Class type, Object instance) {
+        ResteasyProviderFactory.getInstance();
+        ResteasyProviderFactory.getContextData(Dispatcher.class)
+            .getDefaultContextObjects()
+            .put(type, instance);
+    }
 
-	@Override
-	public void pushContext(Class type, Object instance) {
-		ResteasyProviderFactory.getInstance();
-		ResteasyProviderFactory.pushContext(type, instance);
-	}
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    public void pushContext(Class type, Object instance) {
+        ResteasyProviderFactory.getInstance();
+        ResteasyProviderFactory.pushContext(type, instance);
+    }
 
-	@Override
-	public void clearContextData() {
-		ResteasyProviderFactory.getInstance();
-		ResteasyProviderFactory.clearContextData();
-	}
+    @Override
+    public void clearContextData() {
+        ResteasyProviderFactory.getInstance();
+        ResteasyProviderFactory.clearContextData();
+    }
 
 }
