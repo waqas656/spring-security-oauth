b0ee3bbbfe331c0a3a55d19fbd6a455c725c1760
Loredana <lore.crusoveanu@gmail.com>
2020-10-17 11:24:12 +0300

    fix build, update boot version

--- oauth-legacy/oauth-ui-password-angularjs-legacy/src/main/java/com/baeldung/config/SameSiteConfig.java
+++ oauth-legacy/oauth-ui-password-angularjs-legacy/src/main/java/com/baeldung/config/SameSiteConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class SameSiteConfig implements WebMvcConfigurer {
     @Bean
