9c951be9adb217acebcbec7a0f2fa8000f8a0d12
anuragkumawat <anuragkumawat7@gmail.com>
2022-11-04 13:57:24 +0530

    JAVA-15694 Update oauth-sso module under spring-security-oauth repo to remove usage of deprecated WebSecurityConfigurerAdapter

--- oauth-sso/sso-resource-server/src/main/java/com/baeldung/resource/spring/SecurityConfig.java
+++ oauth-sso/sso-resource-server/src/main/java/com/baeldung/resource/spring/SecurityConfig.java
@@ -1,27 +1,28 @@
 package com.baeldung.resource.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors()
             .and()
-              .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
-                  .hasAuthority("SCOPE_read")
-                .antMatchers(HttpMethod.POST, "/api/foos")
-                  .hasAuthority("SCOPE_write")
-                .anyRequest()
-                  .authenticated()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
+            .hasAuthority("SCOPE_read")
+            .antMatchers(HttpMethod.POST, "/api/foos")
+            .hasAuthority("SCOPE_write")
+            .anyRequest()
+            .authenticated()
             .and()
-              .oauth2ResourceServer()
-                .jwt();
-    }//@formatter:on
-
+            .oauth2ResourceServer()
+            .jwt();
+        return http.build();
+    }
 }
